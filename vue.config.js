b63618678af302eb55ef0ff8c73c18c9c5d8c610
zukhouzi <103362391+zukhouzi@users.noreply.github.com>
2022-10-02 01:54:45 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
       entry: 'src/main.js',
       template: 'public/index.html',
       filename: 'index.html',
-      title: 'YesPlayMusic',
+      title: 'NiCE Music',
       chunks: ['main', 'chunk-vendors', 'chunk-common', 'index'],
     },
   },
